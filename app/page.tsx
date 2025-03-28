f7f0df34d78495b28df5fdcd9b76fb6e87e9f34d
AjaySisodiyaa <141384135+AjaySisodiyaa@users.noreply.github.com>
2025-03-28 20:21:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 import Posts from "@/components/Posts";
 
-const getPost = async () => {
-  const res = await fetch("http://localhost:3000/api/postjson");
-
-  if (!res.ok) {
-    throw new Error("Failed to get data");
-  }
-  return res.json();
-};
 export default async function Home() {
-  const posts = await getPost();
+
   return (
     <div className="w-full h-full text-black bg-white ">
-      <Posts posts={posts} />
+      <Posts />
     </div>
   );
 }
